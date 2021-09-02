ff03dc89e53c9acf8bffa0901bcef55f6c163798
tushergoon <tushergoondip@gmail.com>
2021-09-02 19:51:17 +0600

    almost done

--- js/apps.js
+++ js/apps.js
@@ -1,13 +1,17 @@
 const searchResult = document.getElementById('search-result');
+const result = document.getElementById('result');
 const searchBook = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
   // console.log(searchText);
-
+  if (searchText === ' ') {
+    result.innerText = 'You have to write something to search!';
+  }
   // clear data
   searchField.value = ' ';
   // load data
   const url = `https://openlibrary.org/search.json?q=${searchText}`
+
 
   fetch(url)
     .then(res => res.json())
@@ -20,12 +24,18 @@ const displaySearchResult = docs => {
 
   searchResult.textContent = ' ';
   // forEach
-  docs.forEach(doc => {
+  if (docs.length === 0) {
+    result.innerText = 'Showing top 100 results';
+  }
+  else {
+    result.innerText = `Showing top result:${docs.length}`;
 
-    console.log(doc);
-    const div = document.createElement('div');
-    div.classList.add('col');
-    div.innerHTML = `
+    docs.forEach(doc => {
+
+      console.log(doc);
+      const div = document.createElement('div');
+      div.classList.add('col');
+      div.innerHTML = `
     <div class="card h-100 border border-4">
            <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top p-3 h-100 w-100" alt="...">
            <div class="card-body">
@@ -36,6 +46,7 @@ const displaySearchResult = docs => {
           </div>
         </div>
     `;
-    searchResult.appendChild(div);
-  });
+      searchResult.appendChild(div);
+    });
+  }
 }
