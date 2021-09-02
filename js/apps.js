64bbaf759e9e42fd0555bfec7ec00ae0f53ff197
tushergoon <tushergoondip@gmail.com>
2021-09-02 18:53:11 +0600

    last

--- js/apps.js
+++ js/apps.js
@@ -1,8 +1,12 @@
+const searchResult = document.getElementById('search-result');
 const searchBook = () => {
   const searchField = document.getElementById('search-field');
   const searchText = searchField.value;
-  console.log(searchText);
+  // console.log(searchText);
+
+  // clear data
   searchField.value = ' ';
+  // load data
   const url = `https://openlibrary.org/search.json?q=${searchText}`
 
   fetch(url)
@@ -12,18 +16,23 @@ const searchBook = () => {
 
 
 const displaySearchResult = docs => {
-  const searchResult = document.getElementById('search-result');
+
+
+  searchResult.textContent = ' ';
   // forEach
   docs.forEach(doc => {
+
     console.log(doc);
     const div = document.createElement('div');
     div.classList.add('col');
     div.innerHTML = `
     <div class="card h-100 border border-4">
-           <img src="..." class="card-img-top" alt="...">
+           <img src="https://covers.openlibrary.org/b/id/${doc.cover_i}-M.jpg" class="card-img-top p-3 h-100 w-100" alt="...">
            <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            <h2 class="card-title">${doc.title}</h2>
+            <p class="card-text">Author : ${doc.author_name}</p>
+            <p class="card-text">First Published : ${doc.first_publish_year}</p>
+            <p class="card-text">publisher : ${doc.publisher}</p>
           </div>
         </div>
     `;
